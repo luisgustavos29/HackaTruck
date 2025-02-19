6adda8839e10ecfe0e364f6fb38b6f20ecd64096
Luís Gustavo Silva <145591873+luisgustavos29@users.noreply.github.com>
2025-02-19 10:59:14 -0300

    Add files via upload

---
+++ Desafio1/Desafio1/Desafio1App.swift
@@ -0,0 +1,17 @@
+//
+//  Desafio1App.swift
+//  Desafio1
+//
+//  Created by Turma01-19 on 19/02/25.
+//
+
+import SwiftUI
+
+@main
+struct Desafio1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
